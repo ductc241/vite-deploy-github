9ab080326929163f052e9e2e64eea91f92022477
ductc241 <tacongduc123@gmail.com>
2023-05-17 20:56:30 +0700

    setup project

---
+++ src/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis iure
+        similique provident, mollitia officiis temporibus praesentium tempora
+        minus. Odit sequi ullam, veritatis aliquam vero mollitia dolorum officia
+        quaerat a ab.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
