483317f5a23e26235ba2969727f871a50b230258
ductc241 <tacongduc123@gmail.com>
2023-05-17 22:46:55 +0700

    chore: update router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import About from "./About";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<Home />} />
+        <Route path="vite-deploy-github">
+          <Route path="introduce">
+            <Route index path="about" element={<About />} />
+          </Route>
+
+          <Route path=":id" element={<Home />} />
+          {/* more routes */}
+        </Route>
       </Routes>
     </BrowserRouter>
   );
